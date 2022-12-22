f7175bfadaf138640616f4c45a57f65e7b1ea975
elchinbaba <babalielchin@gmail.com>
2022-12-22 21:49:48 +0400

    first approach

--- test.js
+++ test.js
@@ -1,82 +1,85 @@
-function operate(a, b) {
-    if(operator === "+"){
-        return a + b;
-    }
-
-    else if(operator === "-"){
-        return a - b;
-    }
-
-    else if(operator === "x"){
-        return a * b;
-    }
-
-    else if(operator === "÷"){
-        return a / b;
-    }
-
-    else{
-        return a % b;
-    }
+function operate(a, b, operation) {
+    return eval(`${a} ${operation} ${b}`);
 }
 
-const allButtons = document.querySelectorAll("button");
-const numberButtons = document.querySelectorAll(".number");
+const digitButtons = document.querySelectorAll(".digit");
 const operatorButtons = document.querySelectorAll(".operator");
-let screen = document.querySelector(".belowWindow");
+const screen = document.getElementById("belowWindow");
+
 let firstNum = null;
 let secondNum = null;
-let operatorCount = 0;
 let operator = null;
-let result = null;
-let previousOperatorCount = 0;
 
-numberButtons.forEach(button => button.addEventListener("click", function(){
-    if(firstNum === null){
-        if(screen.textContent === "0" && button.textContent !== "."){
-            screen.textContent = "";
-        }
-        screen.textContent += button.textContent;
+let hasBeenCalculated = false; // a variable to control calculation and prevent the same recalculations
+
+const cleanScreen = () => { screen.textContent = "0"; }; // a function for cleaning screen
+
+digitButtons.forEach(digit => digit.addEventListener("click", function(){
+    if(screen.textContent === "0"){
+        screen.textContent = "";
+    }
+    screen.textContent += digit.textContent;
+}));
+
+operatorButtons.forEach(operatorButton => operatorButton.addEventListener("click", function(){
+
+    hasBeenCalculated = false; // once new operation started, calculation is cleared
+
+    operator = operatorButton.textContent;
+
+    // defining the first number
+    if (firstNum === null) {
+        firstNum = +screen.textContent; // + operator in front of the variable is similar with Number(variable)
+
+        cleanScreen();
+
+    }
+    // defining the second number and calculating the result
+    else if (secondNum === null) {
+        secondNum = +screen.textContent;
+
+        screen.textContent = operate(firstNum, secondNum, operator);
+
+        // once calculation is held, the relevant variable assignment is being done
+        hasBeenCalculated = true;
+    }
+    //after calculation result value is assigned to the first number
+    else if (firstNum !== null && secondNum !== null) {
+        // comes back to the start of the second situation as a recursion like fibonacci numbers
+        firstNum = +screen.textContent; // or result
+        secondNum = null;
+
+        cleanScreen();
+    }
+}));
+
+document.getElementById("calculate").addEventListener("click", function(){
+    if (firstNum !== null && !hasBeenCalculated) {
+        secondNum = +screen.textContent;
+
+        screen.textContent = operate(firstNum, secondNum, operator);
+
+        hasBeenCalculated = true;
+
+        firstNum = null;
+        secondNum = null;
+    }
+});
+
+document.getElementById("clear-all").addEventListener("click", (event) => {
+    firstNum = null;
+    secondNum = null;
+
+    cleanScreen();
+});
+
+document.getElementById("clear").addEventListener("click", (event) => {
+    if (secondNum !== null) {
+        secondNum = null;
+    }
+    else {
+        firstNum = null;
     }
 
-    if(firstNum !== null){
-        if(operatorCount !== previousOperatorCount){
-            operatorCount++;
-            screen.textContent = ""; 
-        }
-        screen.textContent += button.textContent;
-    }
-}))
-
-operatorButtons.forEach(button => button.addEventListener("click", function(){
-    if(operatorCount === 0){
-        previousOperatorCount = operatorCount;
-        operatorCount++;
-        firstNum = Number(screen.textContent);
-    }
-
-    if(operatorCount === 1){
-        previousOperatorCount = operatorCount;
-        secondNum = Number(screen.textContent);
-        operatorCount--;
-        if(firstNum !== null && secondNum !== null){
-            result = operate(firstNum, secondNum);
-            screen.textContent = "";
-            screen.textContent = `${result}`;
-            secondNum = null;
-            firstNum = result;
-        }
-    }
-
-    operator = button.textContent;
-    console.log(`firstnum is ${firstNum}`);
-    console.log(`secondNum is ${secondNum}`);
-}))
-
-document.querySelector(".equal").addEventListener("click", function(){
-        result = operate(firstNum, secondNum);
-        screen.textContent = result;
-        return result;
+    cleanScreen();
 })
-
-
